8e60b78b9cae1c7240f1b9d41ddd72b7253ac903
sandwichzzy <zzy15198@163.com>
2025-08-17 11:37:13 +0800

    导入钱包组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,11 @@
-'use client'
+"use client";
 
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
-
+export default function Home() {
   return (
-    <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <ConnectButton />
+    </div>
+  );
 }
-
-export default App
